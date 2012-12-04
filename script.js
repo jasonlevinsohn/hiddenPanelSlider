aa42f9b43da90d445423c98350a0278ac65fd308
Jason Levinsohn <jlevinsohn@endgames.us>
2012-12-04 17:48:21 -0500

    closing the gap to awesomeness

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready( function() {
   //?????????Is it because it is a reference and primative????????????
   window.halfOpen = false;
   window.thirdOpen = false;
+  window.leftToCloseToo = 5;
 
 
   var main = $('#main');
@@ -15,9 +16,22 @@ $(document).ready( function() {
   half.hide();
   third.hide();
 
+  //Resize all the boxes
+  resizeBox(main);
+  resizeBox(half);
+  resizeBox(third);
+
   /*
-  * 
+  * Make all the boxes the size of the browser window
+  * Params:
+  * box = the box to be resized
   */
+  function resizeBox(box) {
+    var h = $(window).height() - 10;
+    var w = $(window).width() - 10;
+    box.height(h);
+    box.width(w);
+  }
 
   /*
   * Toggle the box shadow effect to make the div appear as if
@@ -28,12 +42,24 @@ $(document).ready( function() {
   */
   function toggleBox(t, b) {
     if(t) {
-      b.css('box-shadow', '5px 5px 20px 5px #333');
+      b.css('box-shadow', '2px 5px 20px 5px #333');
     } else {
       b.css('box-shadow', '0px 0px 0px 0px #000');
     }
   }
 
+
+  //Move Main Page back to the start
+  $('#homeTab').on('click', function() {
+    if(window.halfOpen || window.thirdOpen) {
+      window.halfOpen = false;
+      window.thirdOpen = false;
+      main.animate({'left' : window.leftToCloseToo}, 'slow', function() {
+        half.hide();
+        third.hide();
+      })
+    }
+  });
 
   //Show the Third Size Open Window
   $('#thirdTab').on('click', function() {
@@ -43,7 +69,7 @@ $(document).ready( function() {
       //Close the Half Window first if it is open
       if(window.halfOpen) {
         window.halfOpen = false;
-        main.animate({'left': 5}, 'slow', function() {
+        main.animate({'left': window.leftToCloseToo}, 'slow', function() {
           half.hide();
           third.show();
           main.animate({'left': thirdWindowWidth}, 'slow');
@@ -60,7 +86,7 @@ $(document).ready( function() {
     } else {
 
       window.thirdOpen = false;
-      main.animate({'left': 5}, 'slow', function() {
+      main.animate({'left': window.leftToCloseToo}, 'slow', function() {
         toggleBox(window.thirdOpen, main);
         third.hide();
       });
@@ -79,26 +105,27 @@ $(document).ready( function() {
     
     if(!window.halfOpen) {
       window.halfOpen = true;
-      half.show()
-      main.css('box-shadow', '5px 5px 20px 5px #333');
+      
+      toggleBox(window.halfOpen, main);
       //??????????How do we say plus equals variable +=variable????????????????
       
       //Close the Third Pane first if it is open, before opening the other window
       if(window.thirdOpen) {
         window.thirdOpen = false;
-        main.animate({'left': 5}, 'slow', function() {
-          main.animate({'left':  halfWindowWidth}, 'slow', function() {
-            third.hide();
-          });
+        main.animate({'left': window.leftToCloseToo}, 'slow', function() {
+          third.hide();
+          half.show()
+          main.animate({'left':  halfWindowWidth}, 'slow');
         })
       } else {
+        half.show()
         main.animate({'left':  halfWindowWidth}, 'slow');
       }
       
     } else {
       window.halfOpen = false;
       
-      main.animate({'left': 5}, 'slow', function() {
+      main.animate({'left': window.leftToCloseToo}, 'slow', function() {
         toggleBox(window.halfOpen, main);
         half.hide();
       });
