9b3276685fc3ae2d791f23f202942c74103471fc
Jason Levinsohn <jlevinsohn@endgames.us>
2012-12-04 17:15:39 -0500

    got panels sliding

--- script.js
+++ script.js
@@ -14,19 +14,69 @@ $(document).ready( function() {
   //Hide Everything except for main
   half.hide();
   third.hide();
+
+  /*
+  * 
+  */
+
+  /*
+  * Toggle the box shadow effect to make the div appear as if
+  * it is being raised up
+  * Params:
+  * t = toggle(on/off)
+  * b = the box to toggle
+  */
+  function toggleBox(t, b) {
+    if(t) {
+      b.css('box-shadow', '5px 5px 20px 5px #333');
+    } else {
+      b.css('box-shadow', '0px 0px 0px 0px #000');
+    }
+  }
+
+
+  //Show the Third Size Open Window
+  $('#thirdTab').on('click', function() {
+    var thirdWindowWidth = $(window).width() / 3;
+
+    if(!window.thirdOpen) {
+      //Close the Half Window first if it is open
+      if(window.halfOpen) {
+        window.halfOpen = false;
+        main.animate({'left': 5}, 'slow', function() {
+          half.hide();
+          third.show();
+          main.animate({'left': thirdWindowWidth}, 'slow');
+        });
+      } else {
+        third.show();
+        main.animate({'left': thirdWindowWidth}, 'slow');
+      }
+
+      third.show();
+      window.thirdOpen = true;
+      toggleBox(window.thirdOpen, main);
+
+    } else {
+
+      window.thirdOpen = false;
+      main.animate({'left': 5}, 'slow', function() {
+        toggleBox(window.thirdOpen, main);
+        third.hide();
+      });
+    }
+
+  });
   
 
-  
-  $('#homeTab').on('click', function () {
+  //Show the Half Size Open Window
+  $('#halfTab').on('click', function () {
     
     
     //Get the Width of the screen
     var halfWindowWidth = $(window).width() / 2;
-    console.log('Half Width: ' + halfWindowWidth);
-    console.log('Left: ' + main.css('left'));
     
-
-    //Show the Half Size Open Window
+    
     if(!window.halfOpen) {
       window.halfOpen = true;
       half.show()
@@ -37,7 +87,9 @@ $(document).ready( function() {
       if(window.thirdOpen) {
         window.thirdOpen = false;
         main.animate({'left': 5}, 'slow', function() {
-          main.animate({'left':  halfWindowWidth}, 'slow');
+          main.animate({'left':  halfWindowWidth}, 'slow', function() {
+            third.hide();
+          });
         })
       } else {
         main.animate({'left':  halfWindowWidth}, 'slow');
@@ -45,8 +97,11 @@ $(document).ready( function() {
       
     } else {
       window.halfOpen = false;
-      main.css('box-shadow', '0px 0px 0px #000000');
-      main.animate({'left': 5}, 'slow');
+      
+      main.animate({'left': 5}, 'slow', function() {
+        toggleBox(window.halfOpen, main);
+        half.hide();
+      });
     }
   });
 });
